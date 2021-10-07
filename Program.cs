25fbaf7c5acc26baa30a61a69881e8768882a7d8
tjken <tjken@TJK77-CPPC>
2021-10-06 23:31:43 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rewards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rewards = 0;
+            Console.WriteLine("Enter the dataset: ");
+            string data = Console.ReadLine();
+            foreach(string purchase in data.Split(','))
+            {
+                try
+                {
+                    int newpurchase = Convert.ToInt32(purchase);
+                    if (newpurchase > 100)
+                        rewards += 50 + ((newpurchase - 100) * 2);
+                    else if (newpurchase > 50)
+                        rewards += (newpurchase - 50);
+
+                }
+                catch(Exception ex)
+                {
+                    //ignore any messages for this app
+                }
+            }
+            Console.WriteLine("You have earned " + rewards.ToString() + " rewards points.");
+            Console.ReadKey();
+        }
+    }
+}
